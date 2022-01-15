c31209fdc09a0e78a8f5e57cef77355ff29e5b91
Ryan Carins <ryan.carins@riscy.biz>
2022-01-15 15:16:06 +1100

    Remove unused imports

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
-use std::{thread, time};
 use trie::Trie;
+
 fn main() {
     let mut trie = Trie::default();
-
     let file = File::open("words.txt").unwrap();
     let reader = BufReader::new(file);
 
