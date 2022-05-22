c5db3503ce124006baafc02af4de7392279d771b
Ryan Carins <ryan.carins@riscy.biz>
2022-05-22 21:40:11 +1000

    Take multiple words in rest api

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::convert::Infallible;
+use std::string::ParseError;
+use std::{convert::Infallible, str::FromStr};
 use std::sync::Arc;
 
 use warp::Filter;
@@ -8,17 +9,38 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize, Clone)]
 struct Res {
     pub result: bool,
+    pub word: String
+}
+
+struct Words {
+    words: Vec<String>,
+}
+
+impl FromStr for Words {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut words = Words { words: Vec::new() };
+        for word in s.split(",") {
+            words.words.push(word.to_string());
+        }
+
+        Ok(words)
+    }
 }
 
 fn with_trie(trie: Arc<TrimmedVecTrie>) -> impl Filter<Extract = (Arc<TrimmedVecTrie>,), Error = std::convert::Infallible> + Clone {
     warp::any().map(move || trie.clone())
 }
 
-async fn contains(word: String, trie: Arc<TrimmedVecTrie>) -> Result<impl warp::Reply, Infallible>   {
-    let result: Res = Res{result: trie.contains(&word)};
+async fn contains(words: Words, trie: Arc<TrimmedVecTrie>) -> Result<impl warp::Reply, Infallible>   {
+    let mut results: Vec<Res> = Vec::new();
+    for word in words.words {
+        results.push(Res{result: trie.contains(&word), word});
+    }
     return Ok(
         warp::reply::json(
-            &result
+            &results
         )
     );
 }
@@ -36,7 +58,7 @@ async fn main() {
 
     let is_word = warp::get()
         .and(warp::path("is_word"))
-        .and(warp::path::param::<String>())
+        .and(warp::path::param::<Words>())
         .and(warp::path::end())
         .and(with_trie(arc_trie))
         .and_then(contains);
